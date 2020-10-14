9515b2ee9df81ed39b33c90cd053d59382d999ab
Arsh Saxena <66147853+arshsaxena@users.noreply.github.com>
2020-10-15 03:04:23 +0530

    Add files via upload

---
+++ VMC Pygame/VMC Pygame - Class/VMC Pygame - Class 1/VMC Pygame - Class 1.py
@@ -0,0 +1,63 @@
+import pygame
+pygame.init()
+
+#Screen
+screen=pygame.display.set_mode((800, 600))
+
+#Title
+pygame.display.set_caption("VMC Pygame - Class 1 (Snake Game)")
+
+#Variable to check if the game is running or stopped
+running=True
+
+#Snake Body
+snake_pos=[[300, 300], [330, 300], [360, 300], [390, 300]]
+
+#Directions
+step=30
+down=(0, step)
+up=(0, -step)
+right=(step, 0)
+left=(-step, 0)
+direction=left
+
+#Apple
+apple_pos=[300, 340]
+#Timer
+timer=0
+
+#Main Game Loop
+while running:
+    pygame.time.Clock().tick(30)
+    #RGB values = (Red, Blue, Green)
+    screen.fill((0, 100, 0))
+    #Quit
+    for event in pygame.event.get():
+        #Quit
+        if event.type==pygame.QUIT:
+            print("Quit")
+            running=False
+        #Key press
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_DOWN:
+                direction=down
+                print("DOWN")
+            elif event.key==pygame.K_UP:
+                direction=up
+                print("UP")
+            elif event.key==pygame.K_RIGHT:
+                direction=right
+                print("RIGHT")
+            elif event.key==pygame.K_LEFT:
+                direction=left
+                print("LEFT")
+
+    timer+=1
+    if timer==5:
+        snake_pos=[[snake_pos[0][0]+direction[0], snake_pos[0][1]+direction[1]]]+snake_pos[:-1]
+        timer=0
+
+    for x, y in snake_pos:
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), 15)
+
+    pygame.display.update()        
