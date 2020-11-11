9aa1ecb1b281c25561c07dab8b14da81819520c2
Arsh Saxena <66147853+arshsaxena@users.noreply.github.com>
2020-11-11 21:00:46 +0530

    Add files via upload

---
+++ VMC HTML/VMC HTML - Class/VMC HTML - Class 23/VMC HTML - Class 23.js
@@ -0,0 +1,37 @@
+"use strict"; //to execute funtion in modern way 
+/* alert("Hey, Arsh!");
+alert(2+14); */
+This is a single line comment
+/* 
+    This is 
+    a 
+    multi line comment.
+*/
+
+let message; //variable
+message="Hey, Arsh!"
+alert(message)
+
+// message="Hello Arsh!"
+// alert(message)
+
+var user = "Arsh";
+let user1 = "Arsh the Great";
+let age = 15;
+let student = "Class X";
+
+/* 
+    We can't use keywords as varibale name.
+    We can't use any special character in a varibale sign expect "$" (Dollar sign) and "_" (underscore).
+    We can't start a varibale name with a digit. 
+    E.g.,   let a1 (valid)
+            let 1a (invalid)
+    We can only use (A-Z), (a-z), (0-9), and "$" and "_".
+*/
+
+// Variable
+let myFavoriteMovie = "Iron Man";
+myFavoriteMovie = "Iron Man";
+
+// Constants
+const dateofBirth = "02.09.2005";
