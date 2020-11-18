a7887fc4f8e015daf3222fa9abfdd108575b4376
Arsh Saxena <66147853+arshsaxena@users.noreply.github.com>
2020-11-18 21:01:33 +0530

    Add files via upload

---
+++ VMC HTML/VMC HTML - Class/VMC HTML - Class 25/VMC HTML - Class 25.js
@@ -0,0 +1,163 @@
+// let arshIsAStudent = true;
+// console.log(typeof(arshIsAStudent));
+
+// arshIsAStudent = String(arshIsAStudent);
+// console.log("Value of arshIsAStudent:", arshIsAStudent);
+// console.log("Type of arshIsAStudent:", typeof(arshIsAStudent));
+
+// let hostWebpageOnGitHub = true;
+// console.log("Type of hostWebpageOnGitHub:", typeof(hostWebpageOnGitHub));
+// console.log("Type of hostWebpageOnGitHub:", typeof(hostWebpageOnGitHub));
+
+// hostWebpageOnGitHub = Number(hostWebpageOnGitHub);
+// console.log("Value of hostWebpageOnGitHub:", hostWebpageOnGitHub);
+// console.log("Type of hostWebpageOnGitHub:", typeof(hostWebpageOnGitHub));
+
+// let FiveIsGreaterThanTwo = false;
+// console.log("Value of FiveIsGreaterThanTwo:", FiveIsGreaterThanTwo);
+
+// FiveIsGreaterThanTwo = Number(FiveIsGreaterThanTwo);
+// console.log("Value of FiveIsGreaterThanTwo:", FiveIsGreaterThanTwo);
+// console.log("Type of FiveIsGreaterThanTwo:", typeof(FiveIsGreaterThanTwo));
+
+// let studentName = "Arsh";
+// console.log("Value of studentName:", studentName);
+// console.log("Type of studentName:", typeof(studentName));
+
+// studentName = Number(studentName);
+// console.log("Value of studentName:", studentName);
+// console.log("Type of studentName:", typeof(studentName));
+
+// let num = "100";
+// console.log("Value of num:", num);
+// console.log("Type of num:", typeof(num));
+
+// num = Number(num);
+// console.log("Value of num:", num);
+// console.log("Type of num:", typeof(num));
+
+// let num1 = "50";
+// console.log("Value of num1:", num1);
+// console.log("Type of num1:", typeof(num1));
+
+// num1 = Number(num1);
+// console.log("Value of num1:", num1);
+// console.log("Type of num1:", typeof(num1));
+
+// console.log(num/num1);
+
+// let num2 = 1;
+// console.log("Value of num2:", num2);
+// console.log("Type of num3:", typeof(num2));
+
+// num2 = Boolean(num2);
+// console.log("Value of num2:", num2);
+// console.log("Type of num2:", typeof(num2));
+
+// let num3 = 0;
+// console.log("Value of num3:", num3);
+// console.log("Type of num3:", typeof(num3));
+
+// num3 = Boolean(num3)
+// console.log("Value of num3:", num3);
+// console.log("Type of num3:", typeof(num3));
+
+// let num4 = 10;
+// console.log("Value of num4:", num4);
+// console.log("Type of num4:", typeof(num4));
+
+// num4 = Boolean(num4);
+// console.log("Value of num4:", num4);
+// console.log("Type of num4:", typeof(num4));
+
+// let str = "Arsh" 
+// console.log("Value of str:", str);
+// console.log("Type of str:", typeof(str));
+
+// str = Boolean(str);
+// console.log("Value of str:", str);
+// console.log("Type of str:", typeof(str));
+
+// let str1 = "";
+// console.log("Value of str1:", str1);
+// console.log("Type of str1:", typeof(str1));
+
+// str1 = Boolean(str1);
+// console.log("Value of str1:", str1);
+// console.log("Type of str1:", typeof(str1));
+
+// let str2 = " ";
+// console.log("Value of str2:", str2);
+// console.log("Type of str2:", typeof(str2));
+
+// str2 = Boolean(str2);
+// console.log("Value of str2:", str2);
+// console.log("Type of str2:", typeof(str2));
+
+// Operators
+// Urinary operator (only minus(-))
+// -5
+
+// Binary operators (+, -, *, /, %, and **)
+// "/" = Quotient and "%" = Remainder
+// 7+8
+// 9*20
+
+// let num5 = 10;
+// console.log("Value of num4:", num5);
+// num5 = -num5;
+// console.log("Value of num4:", num5);
+
+// let num6 = 14;
+// let num7 = 2;
+// console.log("Value of num6 + num7:", num6 + num7);
+// console.log("Value of num6 - num7:", num6 - num7);
+// console.log("Value of num6 * num7:", num6 * num7);
+// console.log("Value of num6 / num7:", num6 / num7);
+// console.log("Value of num6 ** num7:", num6 ** num7);
+// console.log("Value of num6 % num7:", num6 % num7);
+
+// Prefix and Postfix operatiors
+// let x = 1;
+// console.log("Value of x:", x)
+// x++
+// console.log("Value of x:", x)
+
+// let y = 1;
+// console.log("Value of y:", y)
+// ++y
+// console.log("Value of y:", y)
+
+// let x = 1;
+// console.log("Value of x:", x) // 1
+// let z = x++ // Postfix operator
+// console.log("Value of x:", x) // 2
+// console.log("Value of y:", z) // 1
+
+// let y = 1;
+// console.log("Value of y:", y) // 1
+// let w = ++y // Prefix operator
+// console.log("Value of y:", y) // 2
+// console.log("Value of w:", w) // 2
+
+// let x = 1;
+// console.log("Value of x:", x)
+// x--
+// console.log("Value of x:", x)
+
+// let y = 1;
+// console.log("Value of y:", y)
+// --y
+// console.log("Value of y:", y)
+
+let x = 1;
+console.log("Value of x:", x) 
+let z = x-- // Postfix operator
+console.log("Value of x:", x) 
+console.log("Value of y:", z) 
+
+let y = 1;
+console.log("Value of y:", y) 
+let w = --y // Prefix operator
+console.log("Value of y:", y)
+console.log("Value of w:", w) 
