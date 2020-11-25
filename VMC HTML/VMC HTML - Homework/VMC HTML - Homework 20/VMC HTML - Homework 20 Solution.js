3615f284267818b16e31d7a65e452df8d49b282f
Arsh Saxena <66147853+arshsaxena@users.noreply.github.com>
2020-11-25 14:41:37 +0530

    Add files via upload

---
+++ VMC HTML/VMC HTML - Homework/VMC HTML - Homework 20/VMC HTML - Homework 20 Solution.js
@@ -0,0 +1,19 @@
+let welcome = "Welcome! You will be asked to fill some information. Please fill them.";
+alert(welcome);
+console.log(welcome);
+
+fname = prompt("What is your first name?");
+lname = prompt("What is your last name?");
+console.log(fname, lname, "is your name.");
+
+age = prompt("What is your age?");
+console.log("You are", age, "years old.");
+
+htown = prompt("What is your hometown?");
+console.log(htown, "is your hometwon.");
+
+nationality = prompt("What is your nationality?");
+console.log(nationality, "is your nationality.");
+
+thanks = "Thank you for filling your details."
+console.log(thanks);
