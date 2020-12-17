f2e7c7c82aeecffbd3529963bf688ba5556451ed
Arsh Saxena <66147853+arshsaxena@users.noreply.github.com>
2020-12-18 00:21:24 +0530

    Add files via upload

---
+++ VMC HTML/VMC HTML - Class/VMC HTML - Class 28/VMC HTML - Class 28.js
@@ -0,0 +1,118 @@
+// // alert("Connected!")
+
+// // // Student 1
+// // var x = 2;
+// // var y = 14;
+
+// // console.log(x + y);
+// // console.log(x - y);
+// // console.log(x * y);
+// // console.log(x / y);
+// console.log("Hi Arsh!");
+// console.log("How are you?");
+
+// // // Student 2
+// // var x = 16;
+// // var y = 9;
+
+// // console.log(x + y);
+// // console.log(x - y);
+// // console.log(x * y);
+// // console.log(x / y);
+// console.log("Hi Arsh!");
+// console.log("How are you?");
+
+// // // Student 3
+// // var x = 13;
+// // var y = 3;
+
+// // console.log(x + y);
+// // console.log(x - y);
+// // console.log(x * y);
+// // console.log(x / y);
+// console.log("Hi Arsh!");
+// console.log("How are you?");
+
+// // // Student 4
+// // var x = 3;
+// // var y = 15;
+
+// // console.log(x + y);
+// // console.log(x - y);
+// // console.log(x * y);
+// // console.log(x / y);
+// console.log("Hi Arsh!");
+// console.log("How are you?");
+
+// // // Student 5
+// // var x = 15;
+// // var y = 30;
+
+// // console.log(x + y);
+// // console.log(x - y);
+// // console.log(x * y);
+// // console.log(x / y);
+// console.log("Hi Arsh!");
+// console.log("How are you?");
+
+// /* 
+// SYNTAX
+//     function function_name(arguments(0 - n)){
+//         code........;
+//     };
+//     function_name();
+// */
+
+// function add(x, y){
+//     console.log(x + y);
+// };
+// // add(14, 2);
+// // add(9, 16);
+
+// function sub(x, y){
+//     console.log(x - y);
+// };
+// // sub(14, 2);
+// // sub(9, 16);
+
+// function mul(x, y){
+//     console.log(x * y);
+// };
+// // mul(14, 2);
+// // mul(9, 16);
+
+// function div(x, y){
+//     console.log(x / y);
+// };
+// // div(14, 2);
+// // div(9, 16);
+
+// function greeting(){
+//     console.log("Hi Arsh!");
+//     console.log("How are you?");
+// };
+// // greeting();
+
+// // var a = parseInt(prompt());
+// // var b = parseInt(prompt());
+// // add(a, b);
+// // sub(a, b);
+// // mul(a, b);
+// // div(a, b);
+// // greeting();
+
+// function square(h){
+//     console.log(h * h);
+//     // console.log(h ** 2)
+// }
+
+// var sq = parseInt(prompt())
+// square(sq);
+
+// function add(x, y){
+//     return(x + y);
+// };
+// var s = add(30, 170);
+// console.log(s)
+// n = add(s, 50);
+// console.log(n)
